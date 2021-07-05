bee9c476ec4ff8523011c01aa08f13761208fc4b
Ambliqui <isiseren@gmail.com>
2021-07-05 14:02:52 +0200

    20210705
    Servlets

---
+++ HolaMundo/src/main/java/com/mycompany/holamundo/Hola.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.holamundo;
+
+/**
+ *
+ * @author Mefisto
+ */
+public class Hola {
+    
+    private String msg;
+    
+    public static void main(String[] args) {
+        String msg = "Hola mundo";
+        System.out.println(msg);
+    }
+}
